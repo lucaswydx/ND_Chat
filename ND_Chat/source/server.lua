663b0f7050a0a33172ad4c270e2293111739f16a
Marshular <marshy0909@gmail.com>
2023-04-22 19:46:10 -0700

    Major update
    - Added DO command to source code since it wasn't in the last update....
    - Added updated 911 command to source code since it wasn't in the last update....
    - Added 311 command
    - Add length check for commands to see if a message was actually put so it doesn't just send something blank

--- ND_Chat/source/server.lua
+++ ND_Chat/source/server.lua
@@ -1,52 +1,6 @@
 NDCore = exports["ND_Core"]:GetCoreObject()
 NDCore.Functions.VersionChecker("ND_Chat", GetCurrentResourceName(), "https://github.com/Andyyy7666/ND_Framework", "https://raw.githubusercontent.com/Andyyy7666/ND_Framework/main/ND_Chat/fxmanifest.lua")
 
-if config["/me"] then
-    RegisterCommand("me", function(source, args, rawCommand, text)
-        local player = source
-        local players = NDCore.Functions.GetPlayers()
-        local playerCoords = GetEntityCoords(GetPlayerPed(player))
-        for serverPlayer, _ in pairs(players) do
-            local targetCoords = GetEntityCoords(GetPlayerPed(serverPlayer))
-            if (#(playerCoords - targetCoords) < 20.0) then
-                TriggerClientEvent("chat:addMessage", serverPlayer, {
-                    color = {255, 0, 0},
-                    args = {"^*^3[ME] " .. players[player].firstName .. " " .. players[player].lastName .. " [" .. players[player].job .. "] (#" .. player .. ")", table.concat(args, " ")}
-                })
-            end
-        end
-    end, false)
-end
-
-if config['/gme'] then
-    RegisterCommand('gme', function(source, args)
-        local player = source
-        local source = source
-        local character = NDCore.Functions.GetPlayer(source)
-
-        TriggerClientEvent('chat:addMessage', -1, { args = {"^*^6[GME] " .. character.firstName .. " " .. character.lastName .. " [" .. character.job .. "] (#" .. player .. ")", table.concat(args, " ")}})
-    end, false)
-end
-
-if config['/twt'] then
-    RegisterCommand('twt', function(source, args)
-        local player = source
-        local source = source
-        local character = NDCore.Functions.GetPlayer(source)
-
-        TriggerClientEvent('chat:addMessage', -1, { args = {"^*^4[Twitter] @" .. character.data.twtName .. " (#" .. player .. ")", table.concat(args, " ")}})
-    end, false)
-end
-
-if config['/ooc'] then
-    RegisterCommand('ooc', function(source, args)
-        local player = source
-        local source = source
-        local character = NDCore.Functions.GetPlayer(source)
-
-        TriggerClientEvent('chat:addMessage', -1, { args = {"^*^2[OOC] " .. GetPlayerName(source) .. " [" .. character.job .. "] (#" .. player .. ")", table.concat(args, " ")}})
-    end, false)
-
 function hasDarkWebPermission(player, players, args)
     for _, department in pairs(config["/darkweb"].canNotSee) do
         if players[player].job == department then 
@@ -70,18 +24,105 @@ function hasDarkWebPermission(player, players, args)
     end
 end
 
-if config["/darkweb"].enabled then
-    RegisterCommand("darkweb", function(source, args, rawCommand)
+if config["/me"] then
+    RegisterCommand("me", function(source, args, rawCommand, text)
         local player = source
         local players = NDCore.Functions.GetPlayers()
-        hasDarkWebPermission(player, players, args)
+        local playerCoords = GetEntityCoords(GetPlayerPed(player))
+        local length = string.len(table.concat(args, " "))
+        for serverPlayer, _ in pairs(players) do
+            local targetCoords = GetEntityCoords(GetPlayerPed(serverPlayer))
+            if (#(playerCoords - targetCoords) < 20.0) and (length > 0) then
+                TriggerClientEvent("chat:addMessage", serverPlayer, {
+                    color = {255, 0, 0},
+                    args = {"^*^3[ME] " .. players[player].firstName .. " " .. players[player].lastName .. " [" .. players[player].job .. "] (#" .. player .. ")", table.concat(args, " ")}
+                })
+            end
+        end
+    end, false)
+end
+
+if config["/do"] then
+    RegisterCommand("do", function(source, args, rawCommand, text)
+        local player = source
+        local players = NDCore.Functions.GetPlayers()
+        local playerCoords = GetEntityCoords(GetPlayerPed(player))
+        local length = string.len(table.concat(args, " "))
+        for serverPlayer, _ in pairs(players) do
+            local targetCoords = GetEntityCoords(GetPlayerPed(serverPlayer))
+            if (#(playerCoords - targetCoords) < 20.0) and (length > 0) then
+                TriggerClientEvent("chat:addMessage", serverPlayer, {
+                    color = {255, 0, 0},
+                    args = {"^*^2[DO] " .. players[player].firstName .. " " .. players[player].lastName .. " [" .. players[player].job .. "] (#" .. player .. ")", table.concat(args, " ")}
+                })
+            end
+        end
+    end, false)
+end
+
+if config['/gme'] then
+    RegisterCommand('gme', function(source, args)
+        local player = source
+        local source = source
+        local character = NDCore.Functions.GetPlayer(source)
+        local length = string.len(table.concat(args, " "))
+        if (length > 0) then
+            TriggerClientEvent('chat:addMessage', -1, { args = {"^*^6[GME] " .. character.firstName .. " " .. character.lastName .. " [" .. character.job .. "] (#" .. player .. ")", table.concat(args, " ")}})
+        end
+    end, false)
+end
+
+if config['/twt'] then
+    RegisterCommand('twt', function(source, args)
+        local player = source
+        local source = source
+        local character = NDCore.Functions.GetPlayer(source)
+        local length = string.len(table.concat(args, " "))
+        if (length > 0) then
+            TriggerClientEvent('chat:addMessage', -1, { args = {"^*^4[Twitter] @" .. character.data.twtName .. " (#" .. player .. ")", table.concat(args, " ")}})
+        end
+    end, false)
+end
+
+if config['/ooc'] then
+    RegisterCommand('ooc', function(source, args)
+        local player = source
+        local source = source
+        local character = NDCore.Functions.GetPlayer(source)
+        local length = string.len(table.concat(args, " "))
+        if (length > 0) then
+            TriggerClientEvent('chat:addMessage', -1, { args = {"^*^2[OOC] " .. GetPlayerName(source) .. " [" .. character.job .. "] (#" .. player .. ")", table.concat(args, " ")}})
+        end
+    end, false)
+end 
+
+if config["/darkweb"].enabled then
+    RegisterCommand("darkweb", function(source, args)
+        local player = source
+        local players = NDCore.Functions.GetPlayers()
+        local length = string.len(table.concat(args, " "))
+        if (length > 0) then
+            hasDarkWebPermission(player, players, args)
+        end
     end, false)
 end
 
 if config["/911"].enabled then
-    RegisterCommand("911", function(source, args, rawCommand)
+    RegisterCommand("911", function(source, args)
         local player = source
-        TriggerClientEvent("ND_Chat:911", -1, GetEntityCoords(GetPlayerPed(player)), table.concat(args, " "))
+        local length = string.len(table.concat(args, " "))
+        if (length > 0) then
+            TriggerClientEvent("ND_Chat:911", -1, GetEntityCoords(GetPlayerPed(player)), table.concat(args, " "))
+        end
     end, false)
 end
+
+if config["/311"].enabled then
+    RegisterCommand("311", function(source, args)
+        local player = source
+        local length = string.len(table.concat(args, " "))
+        if (length > 0) then
+            TriggerClientEvent("ND_Chat:311", -1, GetEntityCoords(GetPlayerPed(player)), table.concat(args, " "))
+        end
+    end, false)
 end
